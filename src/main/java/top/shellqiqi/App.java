1377b1f4da265e42f865e4abf014870cfe1dd6dc
shellqiqi <shellqiqi@outlook.com>
2019-11-28 21:00:45 +0800

    Support Multi-thread

--- src/main/java/top/shellqiqi/App.java
+++ src/main/java/top/shellqiqi/App.java
@@ -1,30 +1,45 @@
 package top.shellqiqi;
 
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App {
-    public static void main(String[] args) {
-        Logger logger = Logger.getLogger("top.shellqiqi.cimtool");
-        try {
-            for (String imgPath : args) {
-                if (imgPath.endsWith(".cim")) {
-                    PNGUtil.writePNG(
-                            imgPath.replaceAll("cim$", "png"),
-                            CIMUtil.readCIM(imgPath)
-                    );
-                } else if (imgPath.endsWith(".png")) {
-                    CIMUtil.writeCIM(
-                            imgPath.replaceAll("png$", "cim"),
-                            PNGUtil.readPNG(imgPath)
-                    );
-                } else {
-                    logger.warning("Unknown suffix");
-                    continue;
+    private static final Logger logger = LogManager.getLogger("CIMTool");
+
+    private static Runnable convert(final String imgPath) {
+        return new Runnable() {
+            public void run() {
+                try {
+                    if (imgPath.endsWith(".cim")) {
+                        PNGUtil.writePNG(
+                                imgPath.replaceAll("cim$", "png"),
+                                CIMUtil.readCIM(imgPath)
+                        );
+                        logger.info(imgPath + " -> PNG");
+                    } else if (imgPath.endsWith(".png")) {
+                        CIMUtil.writeCIM(
+                                imgPath.replaceAll("png$", "cim"),
+                                PNGUtil.readPNG(imgPath)
+                        );
+                        logger.info(imgPath + " -> CIM");
+                    } else {
+                        logger.warn("Unknown suffix");
+                    }
+                } catch (Throwable e) {
+                    logger.error(e.getMessage());
                 }
-                logger.info("Convert Done");
             }
-        } catch (Throwable e) {
-            logger.warning(e.getMessage());
-        }
+        };
+    }
+
+    public static void main(String[] args) {
+        final int nThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService pool = Executors.newFixedThreadPool(nThreads);
+        for (String imgPath : args)
+            pool.execute(convert(imgPath));
+        pool.shutdown();
     }
 }
